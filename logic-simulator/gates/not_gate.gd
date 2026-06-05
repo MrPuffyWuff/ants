5b3395a69b0eb6862f2caf50c23ce13b68902aec
MrPuffyWuff <jasper13.com@gmail.com>
2026-06-05 14:21:53 -0400

    Not Gate setup
    modified the component

--- logic-simulator/gates/not_gate.gd
+++ logic-simulator/gates/not_gate.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 @export var identity : Identity
 
+var input_1 : Node2D
+var input_2 : Node2D
+
+var output : int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +14,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	set_output()
+
+func set_output():
+	var in_1_val : int = input_1.output if input_1 != null else 0
+	var in_2_val : int = input_2.output if input_2 != null else 0
+	output = identity.truth_table[Vector2i(in_1_val, in_2_val)]
