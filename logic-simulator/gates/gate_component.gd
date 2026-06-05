1bbbe7a0cae113f522328667a8ff1bdb1e92bca3
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2026-06-05 16:23:04 -0400

    Restructure Gates Folders & Added gate components to all

--- logic-simulator/gates/gate_component.gd
+++ logic-simulator/gates/gate_component.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 func _ready() -> void:
 	$ColorRect/Name.text = parent.identity.gate_label
-	#queue_redraw()
+	queue_redraw()
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("redraw"):
@@ -12,8 +12,8 @@ func _process(delta: float) -> void:
 
 func _draw():
 	if parent.input_1 != null:
-		var color = Color.GREEN if parent.input_1.output == 1 else Color.BLACK
-		draw_line(Vector2.ZERO, parent.input_1.global_position - parent.global_position, color, 1.0)
+		var color = Color.RED if parent.input_1.output == 1 else Color.BLACK
+		draw_line(Vector2.ZERO, parent.input_1.global_position - parent.global_position, color, 2.0)
 	if parent.input_2 != null:
-		var color = Color.GREEN if parent.input_.output == 1 else Color.BLACK
-		draw_line(Vector2.ZERO, parent.input_2.global_position - parent.global_position, color, 1.0)
+		var color = Color.RED if parent.input_2.output == 1 else Color.BLACK
+		draw_line(Vector2.ZERO, parent.input_2.global_position - parent.global_position, color, 2.0)
---
+++ logic-simulator/gates/xor/xor_gate.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var identity : Identity
+
+@export var input_1 : Node2D
+@export var input_2 : Node2D
+
+var output : int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	set_output()
+
+func set_output():
+	var in_1_val : int = input_1.output if input_1 != null else 0
+	var in_2_val : int = input_2.output if input_2 != null else 0
+	output = identity.truth_table[Vector2i(in_1_val, in_2_val)]
