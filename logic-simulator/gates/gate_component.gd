efa8dfb5e25800dd97e9435caccc781800496f33
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2026-06-05 17:09:14 -0400

    Added High and Low and made more presentable

--- logic-simulator/gates/gate_component.gd
+++ logic-simulator/gates/gate_component.gd
@@ -3,6 +3,10 @@ extends Node2D
 @onready var parent : Node2D = get_parent()
 
 func _ready() -> void:
+	if parent.identity.gate_label == "LOW":
+		$ColorRect.color = Color.BLACK
+	elif parent.identity.gate_label == "HIGH":
+		$ColorRect.color = Color.RED
 	$ColorRect/Name.text = parent.identity.gate_label
 	queue_redraw()
 
@@ -12,8 +16,8 @@ func _process(delta: float) -> void:
 
 func _draw():
 	if parent.input_1 != null:
-		var color = Color.RED if parent.input_1.output == 1 else Color.BLACK
+		var color = Color.BROWN if parent.input_1.output == 1 else Color.DIM_GRAY
 		draw_line(Vector2.ZERO, parent.input_1.global_position - parent.global_position, color, 2.0)
 	if parent.input_2 != null:
-		var color = Color.RED if parent.input_2.output == 1 else Color.BLACK
+		var color = Color.BROWN if parent.input_2.output == 1 else Color.DIM_GRAY
 		draw_line(Vector2.ZERO, parent.input_2.global_position - parent.global_position, color, 2.0)
