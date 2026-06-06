7380a668b487053fda79f16fc72e91925d8b3b9c
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2026-06-05 21:27:29 -0400

    Added OUTPUT gate and Made it more visually appealing

--- logic-simulator/gates/gate_component.gd
+++ logic-simulator/gates/gate_component.gd
@@ -5,10 +5,12 @@ extends Node2D
 var dragging: bool = false
 
 func _ready() -> void:
-	if parent.identity.gate_label == "LOW":
-		$ColorRect.color = Color.BLACK
+	if parent.identity.gate_label == "":
+		$ColorRect.color = Color.STEEL_BLUE
+	elif parent.identity.gate_label == "LOW":
+		$ColorRect.color = Color.DARK_GRAY.darkened(0.80)
 	elif parent.identity.gate_label == "HIGH":
-		$ColorRect.color = Color.RED
+		$ColorRect.color = Color.RED.lightened(0.20)
 	$ColorRect/Name.text = parent.identity.gate_label
 	queue_redraw()
 
@@ -20,12 +22,18 @@ func _process(delta: float) -> void:
 		parent.position = get_global_mouse_position()
 
 func _draw():
+	if parent.identity.gate_label == "":
+		if parent.input_1.output == 1:
+			$ColorRect/Name.text = 'ON'
+		else:
+			$ColorRect/Name.text = 'OFF'
+	
 	if parent.input_1 != null:
 		var color = Color.BROWN if parent.input_1.output == 1 else Color.DIM_GRAY
-		draw_line(Vector2.ZERO, parent.input_1.global_position - parent.global_position, color, 3.0)
+		draw_line(Vector2(-1 * $ColorRect.size.x, 0), parent.input_1.global_position - parent.global_position, color, 3.0)
 	if parent.input_2 != null:
 		var color = Color.BROWN if parent.input_2.output == 1 else Color.DIM_GRAY
-		draw_line(Vector2.ZERO, parent.input_2.global_position - parent.global_position, color, 3.0)
+		draw_line(Vector2(-1 * $ColorRect.size.x, 0), parent.input_2.global_position - parent.global_position, color, 3.0)
 
 func _on_color_rect_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
